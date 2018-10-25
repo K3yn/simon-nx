3b814e3e2b5c5eec16d85e067fcf919ed9611ef2
K3yn <44470230+K3yn@users.noreply.github.com>
2018-10-25 19:20:05 +0200

    Create simon_savegame.h

---
+++ include/simon_savegame.h
@@ -0,0 +1,15 @@
+#ifndef simon_savegame_H
+#define simon_savegame_H
+
+#define SAVEPATH "sdmc:/switch/Simon-NX/"
+#define SAVEFILE "simon_save.dat"
+struct stat s;
+int err;
+const char* savepath;
+char filename[37]; // strlen(SAVEPATH) + strlen(SAVEFILE) + 1
+char linea[100];// 
+char namePlayer[3][12];
+u16 highscore[3];
+void generaSaveGame();
+int leeSaveGame();
+#endif
