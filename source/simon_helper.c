b672fffdfd1d2bfed4fd95b1cbadc0cadadf4051
K3yn <44470230+K3yn@users.noreply.github.com>
2018-10-27 18:00:01 +0200

    Update simon_helper.c

--- source/simon_helper.c
+++ source/simon_helper.c
@@ -5,6 +5,47 @@
 #include "SDL_helper.h"
 #include "simon_helper.h"
 
+void loadIniVars(){
+	contMenuMain=4;
+	contMenuModos=3;
+	numAleatorio=0;
+	contSemaforoSecColores=0;
+	contSemaforoColorOnOff=0;
+	contGameOver=0;
+	t=0;// utilizada para debug
+	semaforoAleatorio=0;
+	vecesSecuencia=0;
+	vecesAleatorio=0;
+	contadorPlayer=0;
+	incRand=0;
+	modoAudio=0;
+	puntos=0;
+	aumentaPuntos=3;
+	contSecuencia=0;
+	bucleSecuencia=0;
+	vecesSecuencia=0;
+	//Modos 
+	modo_menuMain=true;
+	game=false;
+	game40=false;
+	game50=false;
+	game80=false;
+	modo_secuencia_colores=false;
+	modo_touch=false;
+	modo_color_random=false;
+	modo_game_over=false;
+	game_over=false;
+	custom_wait_touch=true;
+	modo_blue=false;
+	button_back=false;
+	activarBoton=false;
+	firstTime=true;
+	firstTimeMSC=true;
+	generaSaveGameFlag=false;
+	modo_input_player=false;
+	salir=false;
+	semaforo=true;	
+}
 void playSound(char color){
 	static const char *color_sound;
 	if(modoAudio==4 || modoAudio==5)
@@ -124,5 +165,102 @@ void turnONcolor(char color){
 			break;
 		default:
 			break;	
+	}
+}
+bool comparaStr (char entrada[],char modelo[]){
+int ind = 0;
+
+while (entrada[ind]!='\0' && modelo[ind]!='\0' && entrada[ind] == modelo[ind]) ind++;
+
+if (entrada[ind]!='\0' || modelo[ind]!='\0')
+   return false;
+
+return true;
+}
+void secuenciaColores(){
+// Secuencia de colores. El contador va de 0 a 3 y se resetea
+	if(semaforo)
+	{	if(bucleSecuencia%2==0)
+		{
+			if(contSecuencia==0)
+			{
+				color='G';
+				contSecuencia++;
+			}
+			else if(contSecuencia==1)
+			{
+			color='R';
+			contSecuencia++;
+			}
+			else if(contSecuencia==2)
+			{
+			color='B';
+			contSecuencia++;
+			}
+			else if(contSecuencia==3)
+			{
+			color='Y';
+			contSecuencia++;
+			contSecuencia=0;
+			}
+			
+			if(bucleSecuencia==4)
+			{
+				bucleSecuencia=0;
+			}
+		}
+		bucleSecuencia++;
+	}
+	
+	if(vecesSecuencia==8)
+	{
+		semaforo=false;
+		color=' ';
+	}
+	vecesSecuencia++;
+}
+void fcGameOver(){
+
+	modo_color_random=false;
+	modo_menuMain=true;
+	modo_juego=false;
+	modo_touch=false;
+	modo_game_over=false;
+	game_over=false;
+	firstTime=true;
+	firstTimeMSC=true;
+	game=false;
+	game40=false;
+	game50=false;
+	modo_secuencia_colores=false;
+	custom_wait_touch=true;
+	modo_input_player=false;
+	semaforo=true;
+	
+	vecesAleatorio=0;
+	contadorPlayer=0;
+	contSemaforoColorOnOff=0;
+	semaforoAleatorio=0;
+	contGameOver=0;
+	puntos=0;
+}
+oid marcador(){
+		char cadena[3];
+		int digito1;
+		int digito0;
+		if(puntos<10){
+			renderTexture(numerosColoresTxt.texture, renderer, 0, 0, 1020, 50, 100, 100);//0
+			renderTexture(numerosColoresTxt.texture, renderer, 0+puntos*95, 0, 1120, 50, 95, 100);//
+		}else{
+			itoa(puntos, cadena, 10);
+			digito0=cadena[1]-48;
+			digito1=cadena[0]-48;
+			renderTexture(numerosColoresTxt.texture, renderer, 0+digito0*95, 0, 1120, 50, 95, 100);//
+			renderTexture(numerosColoresTxt.texture, renderer, 0+digito1*95, 0, 1020, 50, 95, 100);//
+		}
+}
+void limpiaArrayColores(){				
+	for (t=0;t<99;t++){
+		arrayColores[t]='\0';	
 	}
 }
