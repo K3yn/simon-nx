ab88a73099145ef1766f23bc1260e3e159a4f34f
K3yn <44470230+K3yn@users.noreply.github.com>
2018-10-27 15:00:19 +0200

    Update simon_touch.h

--- include/simon_touch.h
+++ include/simon_touch.h
@@ -2,7 +2,16 @@
 #define simon_touch_H
 
 touchPosition Stylus;
-bool custom_wait_touch;
 
+u16 green[4];
+u16 red[4];
+u16 yellow[4];
+u16 blue[4];
+u16 pink[4];
+u16 orange[4];
+u16 violet[4];
+u16 cyan[4];
+
+bool custom_wait_touch;
 bool inBox(touchPosition touch, int x1, int y1, int x2, int y2);
 #endif
